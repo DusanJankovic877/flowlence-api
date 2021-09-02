992858a434af665e6834bc3da97a8fa35329eece
Dusan <dusanjankovic877@gmail.com>
2021-09-02 16:41:26 +0200

    crated route for assoc at api.php, crated  assoc controller, and im geting data form DB, editing it and retuning to front end

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AssociationFormController;
 use App\Http\Controllers\ContactMailController;
 use App\Http\Controllers\DooMailController;
 use App\Http\Controllers\EntrepreneurMailController;
@@ -25,3 +26,4 @@ Route::post('/mail', [ ContactMailController::class, 'store']);
 Route::post('/entrepreneur-mail', [ EntrepreneurMailController::class , 'store']);
 Route::post('/doo-mail', [ DooMailController::class , 'store']);
 Route::post('/get-form-data', [ EntrepreneurFormController::class , 'index']);
+Route::get('/association-form-data', [ AssociationFormController::class , 'index']);
