0a17cda1f83dcad04a9a317f2b4aca6d2134c8a1
Dusan <dusanjankovic877@gmail.com>
2021-11-08 16:30:52 +0100

    At PriceListController i have removed unnecessary code

--- app/Http/Controllers/PriceListFormController.php
+++ app/Http/Controllers/PriceListFormController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
-use App\Models\QuestionOption;
-use App\Models\QuestionType;
 use Illuminate\Http\Request;
 
 class PriceListFormController extends Controller
@@ -38,52 +36,6 @@ class PriceListFormController extends Controller
             $form_data = ['data' => Question::where('title', 'already assoc')->with('question_options', 'question_type')->get()];
             return $form_data;
         }else{return 'that does not exists';}
-        // return $request;
-        $questions = Question::with(['question_options', 'question_type'])->get();
-       
-    
-        return $questions;
-        $path = $request->validate(['path' => 'string']);
-        if($path['path'] === '/price-list/entrepreneur'){
-
-            $services = QuestionOption::where('question_id', 1)->get();
-            $people = QuestionOption::where('question_id', 2)->get();
-            $incomes =  QuestionOption::where('question_id', 3)->get();
-            $extra_incomes =  QuestionOption::where('question_id', 4)->get();
-            $pdvs =  QuestionOption::where('question_id', 5)->get();
-            $payments =  QuestionOption::where('question_id', 6)->get();
-            $clients =  QuestionOption::where('question_id', 7)->get();
-            $cash_registers =  QuestionOption::where('question_id', 8)->get();
-            $e_bankings =  QuestionOption::where('question_id', 9)->get();
-            $form_data = ['services' => $services, 'people' =>  $people, 'incomes' => $incomes, 'extraIncomes' => $extra_incomes, 'item1' => $pdvs, 'item2' => $payments, 'clients' => $clients, 'cashRegisters' => $cash_registers, 'item3' => $e_bankings];
-    
-            return $form_data;
-        }else if($path['path'] == '/price-list/doo'){
-            $services = QuestionOption::where('question_id', 1)->get();
-            $founders = QuestionOption::where('question_id', 10)->get();
-            $people = QuestionOption::where('question_id', 2)->get();
-            $incomes =  QuestionOption::where('question_id', 3)->get();
-            $pdvs =  QuestionOption::where('question_id', 11)->get();
-            $payments =  QuestionOption::where('question_id', 12)->get();
-            $clients =  QuestionOption::where('question_id', 7)->get();
-            $cash_registers =  QuestionOption::where('question_id', 8)->get();
-            $e_bankings =  QuestionOption::where('question_id', 13)->get();
-            $form_data = [
-                'services' => $services, 
-                'founders' => $founders, 
-                'people' =>  $people, 
-                'incomes' => $incomes,
-                'item1' => $pdvs, 
-                'item2' => $payments, 
-                'clients' => $clients, 
-                'cashRegisters' => $cash_registers, 
-                'item3' => $e_bankings
-            ];
-            return $form_data;
-
-
-
-        }
 
     }
 
@@ -104,7 +56,7 @@ class PriceListFormController extends Controller
      * @param  \App\Models\EntrepreneurForm  $entrepreneurForm
      * @return \Illuminate\Http\Response
      */
-    public function show(QuestionOption $entrepreneurForm)
+    public function show(Request $request)
     {
         //
     }
@@ -116,7 +68,7 @@ class PriceListFormController extends Controller
      * @param  \App\Models\EntrepreneurForm  $entrepreneurForm
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, QuestionOption $entrepreneurForm)
+    public function update(Request $request)
     {
         //
     }
